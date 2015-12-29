4aef36eb16595dbbbf1e83d7563fb81b6eceb8ea
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2015-12-29 12:20:21 +0300

    BackwardCompatibilityTests. Additional corrections to changelog.

---
+++ src/test/java/com/milaboratory/mixcr/tests/BackwardCompatibilityTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2014-2015, Bolotin Dmitry, Chudakov Dmitry, Shugay Mikhail
+ * (here and after addressed as Inventors)
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify and distribute any part of this program for
+ * educational, research and non-profit purposes, by non-profit institutions
+ * only, without fee, and without a written agreement is hereby granted,
+ * provided that the above copyright notice, this paragraph and the following
+ * three paragraphs appear in all copies.
+ *
+ * Those desiring to incorporate this work into commercial products or use for
+ * commercial purposes should contact the Inventors using one of the following
+ * email addresses: chudakovdm@mail.ru, chudakovdm@gmail.com
+ *
+ * IN NO EVENT SHALL THE INVENTORS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE, EVEN IF THE INVENTORS HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND THE INVENTORS HAS
+ * NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+ * MODIFICATIONS. THE INVENTORS MAKES NO REPRESENTATIONS AND EXTENDS NO
+ * WARRANTIES OF ANY KIND, EITHER IMPLIED OR EXPRESS, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A
+ * PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY
+ * PATENT, TRADEMARK OR OTHER RIGHTS.
+ */
+package com.milaboratory.mixcr.tests;
+
+import cc.redberry.pipe.CUtils;
+import com.milaboratory.core.sequence.AminoAcidAlphabet;
+import com.milaboratory.core.sequence.AminoAcidSequence;
+import com.milaboratory.mixcr.basictypes.*;
+import com.milaboratory.mixcr.reference.GeneFeature;
+import com.milaboratory.mixcr.reference.LociLibraryManager;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by dbolotin on 29/12/15.
+ */
+public class BackwardCompatibilityTests {
+    @Test
+    public void testBC16Alignments() throws Exception {
+        try (VDJCAlignmentsReader reader = new VDJCAlignmentsReader(BackwardCompatibilityTests.class
+                .getResource("/backward_compatibility/alignments_v1.6.vdjca.gz").getFile(),
+                LociLibraryManager.getDefault())) {
+            int countGood = 0;
+            for (VDJCAlignments vdjcAlignments : CUtils.it(reader)) {
+                AminoAcidSequence aaCDR3 = AminoAcidSequence.translateFromCenter(vdjcAlignments.getFeature(GeneFeature.CDR3).getSequence());
+                if (aaCDR3.codeAt(0) == AminoAcidAlphabet.C &&
+                        aaCDR3.codeAt(aaCDR3.size() - 1) == AminoAcidAlphabet.F)
+                    ++countGood;
+            }
+            Assert.assertEquals(98, countGood);
+        }
+    }
+
+    @Test
+    public void testBC16Cloneset() throws Exception {
+        CloneSet cloneSet = CloneSetIO.read(BackwardCompatibilityTests.class
+                        .getResource("/backward_compatibility/clones_v1.6.clns.gz").getFile(),
+                LociLibraryManager.getDefault());
+        Assert.assertEquals(24, cloneSet.size());
+        int countGood = 0;
+        for (Clone clone : cloneSet.getClones()) {
+            AminoAcidSequence aaCDR3 = AminoAcidSequence.translateFromCenter(clone.getFeature(GeneFeature.CDR3).getSequence());
+            if (aaCDR3.codeAt(0) == AminoAcidAlphabet.C &&
+                    aaCDR3.codeAt(aaCDR3.size() - 1) == AminoAcidAlphabet.F) {
+                ++countGood;
+            }
+        }
+        Assert.assertEquals(cloneSet.size(), countGood);
+    }
+}
