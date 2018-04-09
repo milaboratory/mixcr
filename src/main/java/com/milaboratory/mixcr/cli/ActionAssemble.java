abb99388e811cda989993c82ab23562bf7f846bf
Stanislav Poslavsky <stvlpos@mail.ru>
2018-04-10 00:26:20 +0300

    Minor refactorings of Cli for interoperability with 3rd parties

--- src/main/java/com/milaboratory/mixcr/cli/ActionAssemble.java
+++ src/main/java/com/milaboratory/mixcr/cli/ActionAssemble.java
@@ -38,7 +38,6 @@ import com.milaboratory.cli.Action;
 import com.milaboratory.cli.ActionHelper;
 import com.milaboratory.cli.ActionParametersWithOutput;
 import com.milaboratory.mixcr.assembler.*;
-import com.milaboratory.mixcr.basictypes.Clone;
 import com.milaboratory.mixcr.basictypes.CloneSet;
 import com.milaboratory.mixcr.basictypes.CloneSetIO;
 import com.milaboratory.mixcr.basictypes.VDJCAlignmentsReader;
@@ -55,7 +54,20 @@ import java.util.List;
 import java.util.Map;
 
 public class ActionAssemble implements Action {
-    private final AssembleParameters actionParameters = new AssembleParameters();
+    private final AssembleParameters actionParameters;
+
+    public ActionAssemble(AssembleParameters actionParameters) {
+        this.actionParameters = actionParameters;
+    }
+
+    public ActionAssemble() {
+        this(new AssembleParameters());
+    }
+
+    /**
+     * Assemble report
+     */
+    public final CloneAssemblerReport report = new CloneAssemblerReport();
 
     @Override
     public void go(ActionHelper helper) throws Exception {
@@ -94,7 +106,6 @@ public class ActionAssemble implements Action {
         try (CloneAssembler assembler = new CloneAssembler(assemblerParameters,
                 actionParameters.readsToClonesMapping != null, genes, alignerParameters.getFeaturesToAlignMap())) {
             // Creating event listener to collect run statistics
-            CloneAssemblerReport report = new CloneAssemblerReport();
             report.setStartMillis(beginTimestamp);
             report.setInputFiles(new String[]{actionParameters.getInputFileName()});
             report.setOutputFiles(new String[]{actionParameters.getOutputFileName()});
@@ -163,9 +174,9 @@ public class ActionAssemble implements Action {
     }
 
     @Parameters(commandDescription = "Assemble clones")
-    public static final class AssembleParameters extends ActionParametersWithOutput {
+    public static class AssembleParameters extends ActionParametersWithOutput {
         @Parameter(description = "input_file output_file")
-        public List<String> parameters;
+        public List<String> parameters = new ArrayList<>();
 
         @Parameter(description = "Clone assembling parameters",
                 names = {"-p", "--parameters"})
