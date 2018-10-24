09f7031f8b3316851a8f68a0990ef3017ac6aaf1
Stanislav Poslavsky <stvlpos@mail.ru>
2018-10-24 17:17:38 +0300

    This fixes #298

--- src/main/java/com/milaboratory/mixcr/cli/CommandExport.java
+++ src/main/java/com/milaboratory/mixcr/cli/CommandExport.java
@@ -415,8 +415,8 @@ public abstract class CommandExport<T extends VDJCObject> extends ACommandSimple
         ));
 
         alignments.put("full", Arrays.asList(
-                FieldData.mk("-sequence"),
-                FieldData.mk("-quality"),
+                FieldData.mk("-targetSequences"),
+                FieldData.mk("-targetQualities"),
                 FieldData.mk("-vHitsWithScore"),
                 FieldData.mk("-dHitsWithScore"),
                 FieldData.mk("-jHitsWithScore"),
