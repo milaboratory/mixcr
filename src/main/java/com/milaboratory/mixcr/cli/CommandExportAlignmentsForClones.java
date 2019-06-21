1cf0449fefb4316651d3d689de3d1b1025b44b6d
Stanislav Poslavsky <stvlpos@mail.ru>
2019-06-21 17:23:07 +0300

    Added `exportAlignmentsForClones` action

--- src/main/java/com/milaboratory/mixcr/cli/CommandExportAlignmentsForClones.java
+++ src/main/java/com/milaboratory/mixcr/cli/CommandExportAlignmentsForClones.java
@@ -1,30 +1,18 @@
 package com.milaboratory.mixcr.cli;
 
-import cc.redberry.pipe.CUtils;
 import cc.redberry.pipe.OutputPortCloseable;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.milaboratory.cli.ActionConfiguration;
-import com.milaboratory.cli.PipelineConfiguration;
-import com.milaboratory.core.io.sequence.SequenceRead;
-import com.milaboratory.core.io.sequence.SequenceWriter;
-import com.milaboratory.core.io.sequence.fasta.FastaSequenceWriterWrapper;
-import com.milaboratory.core.io.sequence.fastq.PairedFastqWriter;
-import com.milaboratory.core.io.sequence.fastq.SingleFastqWriter;
 import com.milaboratory.mixcr.basictypes.ClnAReader;
 import com.milaboratory.mixcr.basictypes.VDJCAlignments;
 import com.milaboratory.mixcr.basictypes.VDJCAlignmentsWriter;
-import com.milaboratory.util.CanReportProgress;
-import com.milaboratory.util.SmartProgressReporter;
 import io.repseq.core.VDJCLibraryRegistry;
 import picocli.CommandLine;
 
-import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Supplier;
-import java.util.stream.IntStream;
 
 /**
  *
@@ -45,10 +33,10 @@ public class CommandExportAlignmentsForClones extends ACommandWithSmartOverwrite
     @CommandLine.Option(names = "--id", description = "[cloneId1 [cloneId2 [cloneId3]]]", arity = "0..*")
     public List<Integer> ids = new ArrayList<>();
 
-    @CommandLine.Option(description = "Create separate files for each clone. File with '_clnN' suffix, " +
-            "where N is clone index, will be created for each clone index.",
-            names = {"-s", "--separate"})
-    public boolean separate = false;
+//    @CommandLine.Option(description = "Create separate files for each clone. File with '_clnN' suffix, " +
+//            "where N is clone index, will be created for each clone index.",
+//            names = {"-s", "--separate"})
+//    public boolean separate = false;
 
     @Override
     public ActionConfiguration getConfiguration() {
@@ -62,71 +50,20 @@ public class CommandExportAlignmentsForClones extends ACommandWithSmartOverwrite
     @Override
     public void run1() throws Exception {
         try (ClnAReader clna = new ClnAReader(in, VDJCLibraryRegistry.getDefault());
-             VDJCAlignmentsWriter writer  = new VDJCAlignmentsWriter(getOutput())) {
-            writer.header(clna.getAlignerParameters(), clna.getGenes(), getConfiguration());
-
-            VDJCAlignments firstAlignment;
-            try (OutputPortCloseable<VDJCAlignments> dummyP = clna.readAllAlignments()) {
-                firstAlignment = dummyP.take();
-            }
-
-            if (firstAlignment == null)
-                return;
-
-            if (firstAlignment.getOriginalReads() == null)
-                throwValidationException("Error: original reads were not saved in original .vdjca file: " +
-                        "re-run 'align' with '-OsaveOriginalReads=true' option.");
-
-            int[] cid = getCloneIds();
-            Supplier<IntStream> cloneIds;
-            if (cid.length == 0)
-                cloneIds = () -> IntStream.range(0, clna.numberOfClones());
-            else
-                cloneIds = () -> IntStream.of(cid);
-
-            long totalAlignments = cloneIds.get().mapToLong(clna::numberOfAlignmentsInClone).sum();
-            AtomicLong alignmentsWritten = new AtomicLong();
-            AtomicBoolean finished = new AtomicBoolean(false);
-
-            SmartProgressReporter.startProgressReport("Writing reads", new CanReportProgress() {
-                @Override
-                public double getProgress() {
-                    return 1.0 * alignmentsWritten.get() / totalAlignments;
+             VDJCAlignmentsWriter writer = new VDJCAlignmentsWriter(getOutput())) {
+            writer.header(clna.getAlignerParameters(), clna.getGenes(), getFullPipelineConfiguration());
+            long count = 0;
+            for (int id : getCloneIds()) {
+                OutputPortCloseable<VDJCAlignments> reader = clna.readAlignmentsOfClone(id);
+                VDJCAlignments al;
+                while ((al = reader.take()) != null) {
+                    writer.write(al);
+                    ++count;
                 }
-
-                @Override
-                public boolean isFinished() {
-                    return finished.get();
-                }
-            });
-
-            boolean paired = firstAlignment.getOriginalReads().get(0).numberOfReads() == 2;
-
-            try (SequenceWriter globalWriter = separate ? null : createWriter(paired, out)) {
-                cloneIds.get().forEach(cloneId -> {
-                    try (SequenceWriter individualWriter = globalWriter == null ? createWriter(paired, cloneFile(out, cloneId)) : null) {
-                        SequenceWriter actualWriter = globalWriter == null ? individualWriter : globalWriter;
-                        for (VDJCAlignments alignments : CUtils.it(clna.readAlignmentsOfClone(cloneId))) {
-                            for (SequenceRead read : alignments.getOriginalReads())
-                                actualWriter.write(read);
-                            alignmentsWritten.incrementAndGet();
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
             }
+            writer.setNumberOfProcessedReads(count);
         }
     }
-
-    private static String cloneFile(String fileName, int id) {
-        if (fileName.contains(".fast"))
-            fileName = fileName.replace(".fast", "_cln" + id + ".fast");
-        else fileName += id;
-        return fileName;
-    }
-
-
 
     @JsonAutoDetect(
             fieldVisibility = JsonAutoDetect.Visibility.ANY,
@@ -139,7 +76,8 @@ public class CommandExportAlignmentsForClones extends ACommandWithSmartOverwrite
     public static class ExportAlignmentsConfiguration implements ActionConfiguration {
         public final Set<Integer> cloneIds;
 
-        public ExportAlignmentsConfiguration(Set<Integer> cloneIds) {
+        @JsonCreator
+        public ExportAlignmentsConfiguration(@JsonProperty("cloneIds") Set<Integer> cloneIds) {
             this.cloneIds = cloneIds;
         }
 
