c9cd1974eb2e4c81655ad6633ee6196f98f1c46e
Stanislav Poslavsky <stvlpos@mail.ru>
2022-07-11 13:55:13 +0200

    Update miplots dependence

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,7 +83,7 @@ repositories {
 val milibVersion = " 2.0.0-11-master"
 val repseqioVersion = "1.4.0-5-master"
 val mitoolVersion = "1.1.0-2-main"
-val miplotsVersion = "1.0.0-18-master"
+val miplotsVersion = "1.0.0-19-master"
 val jacksonBomVersion = "2.13.3"
 
 dependencies {
