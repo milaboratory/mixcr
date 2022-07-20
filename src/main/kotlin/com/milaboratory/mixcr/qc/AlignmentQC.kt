92c1a8d976d4736cdd52c81cf02f322a43afcc8f
Stanislav Poslavsky <stvlpos@mail.ru>
2022-07-20 22:40:14 +0200

    better image size guess for exportQc align

--- src/main/kotlin/com/milaboratory/mixcr/qc/AlignmentQC.kt
+++ src/main/kotlin/com/milaboratory/mixcr/qc/AlignmentQC.kt
@@ -131,7 +131,7 @@ object AlignmentQC {
             .legendDirectionVertical()
 
 
-        plt += ggsize(900, 100 + 25 * files.size)
+        plt += ggsize(900, 150 + 35 * files.size)
 
 
         plt
