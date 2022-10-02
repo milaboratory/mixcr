4397b6221d09cdd3e415c3eb0a1f2a200097d5f0
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2022-10-02 18:27:34 +0300

    feat: mitool upgrade; better model for group filter reports

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ repositories {
 val milibVersion = "2.0.0-24-master"
 val repseqioVersion = "1.4.1-28-master"
 val miplotsVersion = "1.0.0-27-master"
-val mitoolVersion = "1.1.0-86-main"
+val mitoolVersion = "1.1.0-87-main"
 val jacksonBomVersion = "2.13.4"
 val redberryPipeVersion = "1.2.0-7-master"
 
