5887d450bd14129f2f2c0f24223b14aec7f73b86
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2022-10-07 12:30:08 +0300

    fix: fixes GeneFeaturesTest

--- src/test/kotlin/com/milaboratory/mixcr/basictypes/GeneFeaturesTest.kt
+++ src/test/kotlin/com/milaboratory/mixcr/basictypes/GeneFeaturesTest.kt
@@ -1,6 +1,7 @@
 package com.milaboratory.mixcr.basictypes
 
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.milaboratory.mitool.helpers.K_OM
 import com.milaboratory.util.GlobalObjectMappers
 import io.kotest.matchers.shouldBe
 import io.repseq.core.GeneFeature.CDR3
@@ -12,15 +13,14 @@ class GeneFeaturesTest {
     @Test
     fun `serialize with one gene feature`() {
         val geneFeatures = GeneFeatures(CDR3)
-        GlobalObjectMappers.getOneLine().writeValueAsString(geneFeatures) shouldBe """["CDR3"]"""
-        GlobalObjectMappers.getOneLine().readValue<GeneFeatures>(""""CDR3"""") shouldBe geneFeatures
-        GlobalObjectMappers.getOneLine().readValue<GeneFeatures>("""["CDR3"]""") shouldBe geneFeatures
+        K_OM.writeValueAsString(geneFeatures) shouldBe """["CDR3"]"""
+        K_OM.readValue<GeneFeatures>("""["CDR3"]""") shouldBe geneFeatures
     }
 
     @Test
     fun `serialize with several gene features`() {
         val geneFeatures = GeneFeatures(listOf(VCDR3Part, JCDR3Part))
-        GlobalObjectMappers.getOneLine().writeValueAsString(geneFeatures) shouldBe """["VCDR3Part","JCDR3Part"]"""
-        GlobalObjectMappers.getOneLine().readValue<GeneFeatures>("""["VCDR3Part","JCDR3Part"]""") shouldBe geneFeatures
+        K_OM.writeValueAsString(geneFeatures) shouldBe """["VCDR3Part","JCDR3Part"]"""
+        K_OM.readValue<GeneFeatures>("""["VCDR3Part","JCDR3Part"]""") shouldBe geneFeatures
     }
 }
