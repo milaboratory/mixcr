852d9d8feb46d9ce03460e6fbf0439887ed86233
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2022-10-18 20:03:18 +0300

    chore: miplots upgrade to 1.1.0 release version

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,7 +100,7 @@ repositories {
 
 val milibVersion = "2.1.0"
 val repseqioVersion = "1.5.0"
-val miplotsVersion = "1.0.0-31-master"
+val miplotsVersion = "1.1.0"
 val mitoolVersion = "1.2.0"
 val jacksonBomVersion = "2.13.4"
 val redberryPipeVersion = "1.3.0"
