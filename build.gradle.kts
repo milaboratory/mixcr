662f65e5c5571e1ea893621f083b53f39b51f1f0
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2022-11-11 10:27:42 +0200

    chore: mitool dep upgrade to release build

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ repositories {
 val milibVersion = "2.1.0"
 val repseqioVersion = "1.5.0"
 val miplotsVersion = "1.1.0"
-val mitoolVersion = "1.2.0-15-main"
+val mitoolVersion = "1.3.0"
 val jacksonBomVersion = "2.14.0"
 val redberryPipeVersion = "1.3.0"
 
