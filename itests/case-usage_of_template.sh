81c3111b12cf9e158ff17d3a845b69834b77bc05
gnefedev <georgynefedev@gmail.com>
2022-11-14 10:56:33 +0100

    fix case-usage_of_template

--- itests/case-usage_of_template.sh
+++ itests/case-usage_of_template.sh
@@ -8,15 +8,15 @@ mixcr analyze generic-tcr-amplicon \
   --floating-left-alignment-boundary \
   --floating-right-alignment-boundary J \
   --add-step assembleContigs \
-  test_{{R}}.fastq result_1
+  test_{{R}}.fastq use_of_templates_1
 
-[[ -f result_1.contigs.clns ]] || exit 1
+[[ -f use_of_templates_1.contigs.clns ]] || exit 1
 
 mixcr align -p generic-tcr-amplicon \
   --species hs \
   --rna \
   --floating-left-alignment-boundary \
   --floating-right-alignment-boundary J \
-  test_{{R}}.fastq result_2.vdjca
+  test_{{R}}.fastq use_of_templates_2.vdjca
 
-[[ -f result_2.vdjca ]] || exit 1
+[[ -f use_of_templates_2.vdjca ]] || exit 1
