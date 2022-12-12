ed2f2734270e1b7ade729251b864943c26be817e
gnefedev <georgynefedev@gmail.com>
2022-12-12 10:48:38 +0100

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,12 +98,12 @@ repositories {
     }
 }
 
-val milibVersion = "2.2.0-9-master"
-val repseqioVersion = "1.6.0"
+val milibVersion = "2.2.0-16-refactoring"
+val repseqioVersion = "1.6.0-3-refactoring"
 val miplotsVersion = "1.2.0"
-val mitoolVersion = "1.4.0-11-main"
+val mitoolVersion = "1.5.0-8-refactoring"
 val jacksonBomVersion = "2.14.1"
-val redberryPipeVersion = "1.3.0"
+val redberryPipeVersion = "1.3.0-5-refactoring"
 
 dependencies {
     api("cc.redberry:pipe:$redberryPipeVersion")
