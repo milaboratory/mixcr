d41ef833cd98d3eb34842bb23d2a39a4081c4cc4
gnefedev <georgynefedev@gmail.com>
2022-12-16 15:02:42 +0100

    fix amino acids line in alignments pretty, add tests

--- src/test/kotlin/com/milaboratory/mixcr/cli/TestMain.kt
+++ src/test/kotlin/com/milaboratory/mixcr/cli/TestMain.kt
@@ -1,8 +1,15 @@
 package com.milaboratory.mixcr.cli
 
+import com.milaboratory.milm.MiXCRMain
 import io.kotest.matchers.shouldBe
 
 object TestMain {
+    init {
+        if (MiXCRMain.lm == null) {
+            MiXCRMain.main()
+        }
+    }
+
     fun execute(args: String) {
         Main.mkCmd().execute(*args.split(" ").toTypedArray()) shouldBe 0
     }
