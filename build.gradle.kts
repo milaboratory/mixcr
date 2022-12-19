23c703f31a63a5e303488053898b92943166ec7e
mizraelson <mizraelson@gmail.com>
2022-12-19 08:45:32 -0800

    singleron preset (#926)
    * singleron preset added
    
    * update mitool version

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,7 +101,7 @@ repositories {
 val milibVersion = "2.2.0-25-master"
 val repseqioVersion = "1.6.0-4-master"
 val miplotsVersion = "1.2.0"
-val mitoolVersion = "1.5.0-26-main"
+val mitoolVersion = "1.5.0-29-main"
 val jacksonBomVersion = "2.14.1"
 val redberryPipeVersion = "1.3.0-9-master"
 
