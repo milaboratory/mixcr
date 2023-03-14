60c405bc6a339f1b15354174d3f62d400b7166dc
gnefedev <georgynefedev@gmail.com>
2023-03-14 12:35:55 +0100

    try to align on known alleles

--- itests/case-search_for_alleles_on_broken_library.sh
+++ itests/case-search_for_alleles_on_broken_library.sh
@@ -49,4 +49,9 @@ mixcr findAlleles \
 
 assert "head -n 1 alleles/report.json | jq -r .statuses.FOUND_KNOWN_VARIANT" "5"
 assert "head -n 1 alleles/report.json | jq -r .statuses.DE_NOVA" "3"
+assert "head -n 1 alleles/report.json | jq -r .statuses.ALIGNED_ON_KNOWN_VARIANT" "2"
+assert "head -n 1 alleles/report.json | jq -r .statuses.NOT_CHANGED_NOT_ENOUGH_INFO" "1"
 assert "head -n 1 alleles/report.json | jq -r '.zygotes.\"2\"'" "1"
+
+#count is too low for this gene
+assert "grep '\*B' alleles/description.tsv" "IGHV4-55*B"
