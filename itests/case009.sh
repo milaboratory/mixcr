ff0514b4fe81a991869f500e3e955ee1d473e59c
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2023-06-06 16:19:56 +0300

    test fixes

--- itests/case009.sh
+++ itests/case009.sh
@@ -29,8 +29,8 @@ mixcr analyze --verbose generic-tcr-amplicon \
   CD4M1_test_R1.fastq.gz CD4M1_test_R2.fastq.gz case9
 
 assert "cat case9.align.report.json | head -n 1 | jq -r .chainUsage.chains.TRA.total" "237736"
-assert "cat case9.assemble.report.json | head -n 1 | jq -r .readsInClones" "199564"
+assert "cat case9.assemble.report.json | head -n 1 | jq -r .readsInClones" "199574"
 assert "cat case9.assembleContigs.report.json | head -n 1 | jq -r .longestContigLength" "277"
 assert "cat case9.assembleContigs.report.json | head -n 1 | jq -r .clonesWithAmbiguousLetters" "1168"
 assert "cat case9.assembleContigs.report.json | head -n 1 | jq -r .assemblePrematureTerminationEvents" "3"
-assert "cat case9.assembleContigs.report.json | head -n 1 | jq -r .finalCloneCount" "25593"
+assert "cat case9.assembleContigs.report.json | head -n 1 | jq -r .finalCloneCount" "25595"
