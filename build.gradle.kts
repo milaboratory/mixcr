c2fa38d1eec476fbcfd96a10bacb22a145b095b3
gnefedev <georgynefedev@gmail.com>
2023-06-24 19:19:33 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,7 +130,7 @@ val toObfuscate: Configuration by configurations.creating {
 
 val obfuscationLibs: Configuration by configurations.creating
 
-val mixcrAlgoVersion = "4.3.0-195-precalculate_fraction"
+val mixcrAlgoVersion = "4.3.0-201-precalculate_fraction"
 val milibVersion = ""
 val mitoolVersion = ""
 val repseqioVersion = ""
