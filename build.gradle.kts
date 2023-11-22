142689ebf2bc56e7f0aff03798ec9469d2202350
gnefedev <georgynefedev@gmail.com>
2023-11-22 16:35:15 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.5.0-39-fixes"
+val mixcrAlgoVersion = "4.5.0-44-VCDR3Mutations"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
