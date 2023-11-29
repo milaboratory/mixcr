22d3a84cb398c7168c71fc545366eef091da87f0
gnefedev <georgynefedev@gmail.com>
2023-11-29 18:44:57 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -142,7 +142,7 @@ val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
 val mitoolVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val repseqioVersion = "2.3.0-8-fix-geneFeatures-parse"
+val repseqioVersion = "2.3.0-18-update-library"
 
 val picocliVersion = "4.6.3"
 val jacksonBomVersion = "2.15.2"
