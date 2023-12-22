5ab3cfa6de37a619256c295f1bee05c0315272c3
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2023-12-22 11:46:32 -0600

    feat: implementation of by-chain consensus assembly

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,7 +134,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.6.0-14-fix-qc"
+val mixcrAlgoVersion = "4.6.0-17-consensus-by-chain"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
