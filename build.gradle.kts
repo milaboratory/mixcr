fd6f9cb73ec0a8510dbaea136820771377e4e0c8
gnefedev <georgynefedev@gmail.com>
2024-02-21 10:26:38 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,7 +134,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.6.0-50-rework_analyze"
+val mixcrAlgoVersion = "4.6.0-65-force-realign-trigger"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
