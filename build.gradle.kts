a0ed1e5fceace1b9d7b41eb1bcbae6f32214c875
gnefedev <georgynefedev@gmail.com>
2024-03-04 13:21:33 +0100

    fix NDNDistanceBetweenTrees

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,7 +134,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.6.0-81-fix-report-name"
+val mixcrAlgoVersion = "4.6.0-83-fix-trees"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
