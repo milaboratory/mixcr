26a4732fc2db57d1c42378eaa028b634028100ca
gnefedev <georgynefedev@gmail.com>
2024-06-27 18:59:01 +0200

    10x: integrate mitool into the preset

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,13 +134,13 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.6.0-192-4-7-0-RC-plus-mitool-integration"
+val mixcrAlgoVersion = "4.6.0-193-4-7-0-RC-plus-mitool-integration"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val mitoolVersion = "2.2.0-66-integration-with-mixcr"
+val mitoolVersion = ""
 // may be blank (will be inherited from mixcr-algo)
 val repseqioVersion = ""
 
