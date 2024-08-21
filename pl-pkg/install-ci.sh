a173eed8850a98ef5787448a0334526e263e748d
Vladimir Antropov <vladimir.antropov@milaboratories.com>
2024-08-21 17:36:20 +0200

    add find zip acrhive function

--- pl-pkg/install-ci.sh
+++ pl-pkg/install-ci.sh
@@ -25,14 +25,30 @@ function log() {
     printf "%s\n" "${*}"
 }
 
-if [ -z "$( ls -A ${ARCHIVE_PATH} )" ]; then
-    log "Artifact folder with MiXCR is empty: '${ARCHIVE_PATH}'"
-    exit 1
-fi	
+function find_zip_archive() {
+    local zip_files
+    IFS=$'\n' zip_files=( $(find "${ARCHIVE_PATH}" -type f -name '*.zip') )
 
+    if [ ${#zip_files[@]} -eq 0 ]; then
+        log "No zip archive found in '${ARCHIVE_PATH}'"
+        return 1
+    elif [ ${#zip_files[@]} -gt 1 ]; then
+        log "More than one zip archive found. Please specify which one to use."
+        return 1
+    else
+        ARCHIVE_PATH="${zip_files[0]}"
+        return 0
+    fi
+}
+
+if ! find_zip_archive; then
+    exit 1
+fi
+
+log "Zip archive found at '${ARCHIVE_PATH}'"
 log "Creating: ${dst_data_dir}"
 
 rm -rf "${dst_data_dir}" # make sure we have no waste to be packed into software package
 mkdir -p "${dst_data_dir}"
 
-cp -av "${ARCHIVE_PATH}"/* "${dst_data_dir}"/
+unzip "${ARCHIVE_PATH}" -d "${dst_data_dir}"
