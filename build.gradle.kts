8dba158629bdddebae81ca63d034c341fc85a6fd
gnefedev <georgynefedev@gmail.com>
2024-08-28 09:59:51 +0200

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,9 +134,9 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-2-fix-freeze"
+val mixcrAlgoVersion = "4.7.0-10-mitool-tag-types"
 // may be blank (will be inherited from mixcr-algo)
-val milibVersion = "3.5.0-1-add-progress-to-fasta"
+val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
