82cdf2fda6b2b618348632085261c26d6033c3c5
gnefedev <georgynefedev@gmail.com>
2024-10-31 20:12:18 +0100

    trees exports

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,7 +134,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-31-fixes"
+val mixcrAlgoVersion = "4.7.0-35-exports-for-trees"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
