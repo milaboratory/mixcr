f751638d847fd5ff1928dc3a55e36245a61e816c
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-04-19 19:10:47 +0300

    Update Gradle wrapper to version 8.13, adjust mixcrAlgoVersion and mitoolVersion.

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,13 +134,13 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-39-develop"
+val mixcrAlgoVersion = "4.7.0-40-develop"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val mitoolVersion = "2.3.0-17-mitool-tag-types"
+val mitoolVersion = "2.3.1-8-main"
 // may be blank (will be inherited from mixcr-algo)
 val repseqioVersion = ""
 
