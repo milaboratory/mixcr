2079380cb13e82490cb40e0dd82e29469b4b41e5
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-07-23 22:48:28 +0200

    Build fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,13 +134,13 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-44-develop"
+val mixcrAlgoVersion = "4.7.0-45-develop"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val mitoolVersion = "2.3.1-9-main"
+val mitoolVersion = ""
 // may be blank (will be inherited from mixcr-algo)
 val repseqioVersion = "2.5.0-14-master"
 
