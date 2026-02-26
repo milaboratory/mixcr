25903bbdf9ae264bee87cedd9e729953bb65efdc
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2026-02-26 21:18:56 +0100

    Update mixcr-algo to 4.7.0-58-develop
    Includes fix for D gene cross-chain alignment on light chains.

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,7 +134,7 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-56-develop"
+val mixcrAlgoVersion = "4.7.0-58-develop"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
