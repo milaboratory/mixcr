cf02e24d812e4ae490c697dc2be4da2e9c49f004
Mark Izraelson <mizraelson@gmail.com>
2026-03-21 18:54:16 -0400

    Update mitool to 2.3.1-42-main and mixcr-algo to 4.7.0-61-develop
    mitool 2.3.1-42-main adds maxMismatches parameter to MapTags for fuzzy
    barcode matching (2+ mismatches via SequenceTreeMap prefix tree),
    validated on Visium HD ONT TCR data.

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,13 +134,13 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-56-develop"
+val mixcrAlgoVersion = "4.7.0-61-develop"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val mitoolVersion = "2.3.1-36-main"
+val mitoolVersion = "2.3.1-42-main"
 // may be blank (will be inherited from mixcr-algo)
 val repseqioVersion = "2.5.0-14-master"
 
