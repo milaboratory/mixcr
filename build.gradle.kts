435a0b61108b58c48c7e5699070940bb61ac6694
Mark Izraelson <mizraelson@gmail.com>
2026-03-22 22:33:27 -0400

    Update mitool to 2.3.1-44-main and mixcr-algo to 4.7.0-63-develop

--- build.gradle.kts
+++ build.gradle.kts
@@ -134,13 +134,13 @@ val toObfuscate: Configuration by configurations.creating {
 val obfuscationLibs: Configuration by configurations.creating
 
 
-val mixcrAlgoVersion = "4.7.0-61-develop"
+val mixcrAlgoVersion = "4.7.0-63-develop"
 // may be blank (will be inherited from mixcr-algo)
 val milibVersion = ""
 // may be blank (will be inherited from mixcr-algo or milib)
 val miuVersion = ""
 // may be blank (will be inherited from mixcr-algo)
-val mitoolVersion = "2.3.1-42-main"
+val mitoolVersion = "2.3.1-44-main"
 // may be blank (will be inherited from mixcr-algo)
 val repseqioVersion = "2.5.0-14-master"
 
